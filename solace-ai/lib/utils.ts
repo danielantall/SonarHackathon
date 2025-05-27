4b41bc4bc63065b144690a8d6c27fcf252729b72
Osarumen Edmund Izedonmwen <68762453+rumen-spec@users.noreply.github.com>
2025-05-27 15:24:05 -0400

    journal integration

--- solace-ai/lib/utils.ts
+++ solace-ai/lib/utils.ts
@@ -1,6 +1,43 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios"
+
+const BACKEND = process.env.NEXT_PUBLIC_BACKEND
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function getAllJournals(userId: string) {
+  try {
+    const response = await axios.get(`${BACKEND}/api/journals/`, {
+      params: { user_id: userId }
+    })
+    return response.data
+  } catch (err) {
+    return err
+  }
+}
+
+export async function addJournal(content: string, userId: string){
+  try{
+    const response = await axios.post(`${BACKEND}/api/addjournal`, {user_id: userId, content: content})
+    return response.data
+  } catch(err){
+    return err
+  }
+}
+
+export async function addHabit(name: string, desc: string, targetStreak: number, userId: string){
+  try{
+    const response = await axios.post(`${BACKEND}/api/addHabit`, {
+      user_id: userId,
+      name: name,
+      description: desc,
+      target: targetStreak,
+    })
+    return response.data
+  } catch(err){
+    return err
+  }
 }
